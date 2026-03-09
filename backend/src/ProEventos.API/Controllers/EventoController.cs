714f2bf2adfe1d28e2ad52cf510d91d64a154bb6
Pablo Quirino <pabloquirino17@gmail.com>
2026-03-09 12:20:08 -0300

    Add initial project files and configuration for ProEventos API
    - Created project.nuget.cache to manage NuGet package dependencies.
    - Added global.json to specify the .NET SDK version (5.0.408).
    - Initialized solution file (pro-eventos.sln) with backend and ProEventos.API projects.

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using ProEventos.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProEventos.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext context;
+        public EventoController(DataContext context) //"ASP.NET, me entregue um DataContext já configurado com o banco."
+        {
+            this.context = context;//"Aqui está."
+            
+        }
+ 
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetbyId(int id)
+        {
+            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
