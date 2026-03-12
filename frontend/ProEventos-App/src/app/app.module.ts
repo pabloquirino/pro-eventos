d565435f9d9932874c4941653cb34cc6fa92d3df
Pablo Quirino <pabloquirino17@gmail.com>
2026-03-11 22:34:41 -0300

    feat(eventos): adiciona filtro de eventos e centraliza gitignore do projeto

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     PalestrantesComponent,
     EventosComponent,
-    NavComponent
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()  
+    CollapseModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any[] = [];
+  public eventosFiltrados: any[] = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  toggleImg: boolean = true;
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +23,34 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
+      (response: any) => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       (error) => console.log(error),
+    );
+  }
+
+  public showAndHideImg(): void {
+    this.toggleImg = !this.toggleImg;
+  }
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtrarEventos(value);
+  }
+
+  public filtrarEventos(filtro: string): any[] {
+    filtro = filtro.toLowerCase();
+
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLowerCase().indexOf(filtro) !== -1 ||
+        evento.local.toLowerCase().indexOf(filtro) !== -1,
     );
   }
 }
